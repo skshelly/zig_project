28c64cb97745ba350efd5db6375428d890152dc2
sushil <sushil@jaxon.ai>
2026-02-24 20:44:38 -0500

    add command line args parsing

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,17 @@ pub fn main() !void {
     var buffer: [4096]u8 = undefined;
     var stdout_impl = std.fs.File.stdout().writer(&buffer);
     const stdout = &stdout_impl.interface;
-    try stdout.print("Word Counter\n", .{});
+
+    // get the arguments
+    var args = std.process.args();
+    _ = args.next(); // skip the program name itself
+
+    const filename = args.next() orelse {
+        try stdout.print("Usage: wc <filename>\n", .{});
+        try stdout.flush();
+        return;
+    };
+
+    try stdout.print("File: {s}\n", .{filename});
     try stdout.flush();
 }
